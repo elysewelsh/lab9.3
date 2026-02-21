853d6b057f7402e5686f982faabe58fbcf4d0aea
elysewelsh <elysecwelsh@gmail.com>
2026-02-20 23:03:10 -0500

    improvements to filtering made, but still incorrect.

--- src/App.tsx
+++ src/App.tsx
@@ -9,102 +9,124 @@ import './App.css'
 // import { TaskItem } from './components/TaskItem/TaskItem'
 
 const ogTasks: Task[] = [
-{
-  id: "AC",
-  title: "See Alice Cooper Live",
-  description: "Does this guy know how to party or what?",
-  status: "pending",
-  priority: 'low',
-  dueDate: "November",
-}
+    {
+        id: "AC",
+        title: "See Alice Cooper Live",
+        description: `"Does this guy know how to party or what?"`,
+        status: "pending",
+        priority: 'low',
+        dueDate: "November",
+    }
 ,
-{
-  id: "GR",
-  title: "Refuse Gun Rack",
-  description: "I don't even own ah gun, let alone many guns that would necessitate an entire rack.",
-  status: "completed",
-  priority: 'low',
-  dueDate: "Yesterday",
-}
+    {
+        id: "GR",
+        title: "Refuse Gun Rack",
+        description: `"I don't even own ah gun, let alone many guns that would necessitate an entire rack."`,
+        status: "completed",
+        priority: 'low',
+        dueDate: "Yesterday",
+    }
 ,
-{
-  id: "GA",
-  title: "Play Epic Drum Solo",
-  description: "Thanks. I like to play.",
-  status: "completed",
-  priority: 'high',
-  dueDate: "Last Month",
-}
+    {
+        id: "GA",
+        title: "Play Epic Drum Solo",
+        description: `"Thanks. I like to play."`,
+        status: "completed",
+        priority: 'high',
+        dueDate: "Last Month",
+    }
 ,
-{
-  id: "DE",
-  title: "Visit Delaware",
-  description: "Hi. I'm in...Delaware",
-  status: "in-progress",
-  priority: 'low',
-  dueDate: "2027",
-}
+    {
+        id: "DE",
+        title: "Visit Delaware",
+        description: `"Hi. I'm in...Delaware"`,
+        status: "in-progress",
+        priority: 'low',
+        dueDate: "2027",
+    }
 ,
-{
-  id: "WC",
-  title: "Buy Excalibur",
-  description: "No stairway!",
-  status: "in-progress",
-  priority:'high',
-  dueDate: "ASAP",
-}
+    {
+        id: "WC",
+        title: "Buy Excalibur",
+        description: `"No stairway!"`,
+        status: "in-progress",
+        priority:'high',
+        dueDate: "ASAP",
+    }
 ,
-{
-  id: "BR",
-  title: "Road Trip",
-  description: "If you're gonna spew, spew into this.",
-  status: "pending",
-  priority: 'medium',
-  dueDate:"Tonight",
-}
+    {
+        id: "BR",
+        title: "Road Trip",
+        description: `"If you're gonna spew, spew into this."`,
+        status: "pending",
+        priority: 'medium',
+        dueDate:"Tonight",
+    }
 ]
 
 function App() {
     // const [status, setStatus] = useState('All');
     // const [priority, setPriority] = useState('All');
 
-
+let filteredTasks: Task[] = [];
     
-    const [tasks, setTasks] = useState(ogTasks);
-    const [filterState, setFilterState] = useState({
-      status: "",
-      priority: ""
-    });
+const [tasks, setTasks] = useState(ogTasks);
+
+const [filterState, setFilterState] = useState({
+    status: "",
+    priority: ""
+});
   
 function handleStatusChange (taskId:string, newStatus:TaskStatus) {
-    console.log(taskId,newStatus);
-}
-
-function handleDelete (taskId:string) {
-        setTasks((prevTask) => 
-        prevTask.filter(tasks => tasks.id !== taskId)
-       );
-}
-function handleFilterChange (filters: {status?: TaskStatus, priority?: string}): void {  
-  setFilterState((prevFilterState) => {
-        return { ...prevFilterState, ...filters };
-  })
+    // console.log(taskId,newStatus);
+    setTasks(prevTasks => prevTasks.map(task => task.id === taskId ? {...task, status: newStatus } : task));
 };
 
-const filteredTasks = tasks.filter((task) => {
-    return (
-      ((filterState.status === "") || ( task.status === filterState.status))
-      &&
-      ((filterState.priority === "") || (task.priority === filterState.priority))
-    );
-});
+// setMyArray(prevArray => prevArray.map(item => item.id === itemIdToUpdate ? { ...item, property: newValue } : item))
 
-  return (
-    <>
-    <TaskFilter onFilterChange={handleFilterChange}></TaskFilter>
-    <TaskList tasks={filteredTasks} onStatusChange={handleStatusChange} onDelete={handleDelete} />
-    </>
-  )
+function handleDelete (taskId:string) {
+    setTasks((prevTask) => 
+    prevTask.filter(tasks => tasks.id !== taskId)
+    );
 }
+
+function handleFilterChange (filters: {status?: TaskStatus, priority?: string}): void {  
+    setFilterState((prevFilterState) => {
+        return { ...prevFilterState, ...filters };
+    });
+};
+
+
+function filterTasks (tasks: Task[]) {
+    filterState.status === "" && filterState.priority === "" ? 
+        filteredTasks = tasks : 
+        filterState.status !== "" && filterState.priority !== "" ?
+            filteredTasks = tasks.filter(task => (task.status.includes(filterState.status) && task.priority.includes(filterState.priority))) :
+            filterState.status === "" ?
+                filteredTasks = tasks.filter(task => task.priority.includes(filterState.priority)) :
+                filteredTasks = tasks.filter(task => task.status.includes(filterState.status));
+    return filteredTasks;
+}
+
+// let filteredTasks = tasks.filter(task => task.status.includes(filterState.status))
+
+// const filteredTasks = tasks.filter((task) => {
+//     return (
+//         ((filterState.status === "") || ( task.status === filterState.status))
+//         &&
+//         ((filterState.priority === "") || (task.priority === filterState.priority))
+//     );
+// });
+
+// let result = FRUITS.filter(fruit => fruit.toLowerCase().includes(debouncedQuery.toLowerCase()))
+
+
+    return (
+        <>
+        <TaskFilter onFilterChange={handleFilterChange}></TaskFilter>
+        <TaskList tasks={filterTasks(tasks)} onStatusChange={handleStatusChange} onDelete={handleDelete} />
+        </>
+    );
+};
 
 export default App
